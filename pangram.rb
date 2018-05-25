b3e83f7cace5625db6c132593ba1b844cfae904e
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-25 03:39:01 -0400

    this was a fun one

--- pangram.rb
+++ pangram.rb
@@ -1,4 +1,12 @@
+require 'pry'
+
+ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 class Pangram
   def self.pangram?(text)
+    if text.gsub(' ','').split('').uniq.sort == ALPHABET
+      return true
+    end
   end
 end
--- pangram_test.rb
+++ pangram_test.rb
@@ -19,7 +19,7 @@ class PangramTest < Minitest::Test
   end
 
   def test_pangram_with_only_lower_case
-    skip
+    # skip
     phrase = 'the quick brown fox jumps over the lazy dog'
     result = Pangram.pangram?(phrase)
     assert result, "Expected true, got: #{result.inspect}. #{phrase.inspect} IS a pangram"
